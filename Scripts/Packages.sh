aad6aa79268d34881c957e7113a5f77d7b0a9620
Bulatncq65 <emmanuelBulatncq65@gmail.com>
2026-04-25 01:04:23 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -60,7 +60,7 @@ UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
 
-UPDATE_PACKAGE "myscale" "MmDawN/openwrt-tailscale" "main" "" "tailscale"
+UPDATE_PACKAGE "tailscale" "MmDawN/openwrt-tailscale" "main" "pkg"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
