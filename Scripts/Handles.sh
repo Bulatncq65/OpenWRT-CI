310b5256e5ae441b266e64b27eb7c5355a32ee63
Bulatncq65 <emmanuelBulatncq65@gmail.com>
2026-09-14 15:45:37 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -292,10 +292,10 @@ FEEDS_PACKAGES="$PKG_PATH/../feeds/packages"
 TS_FILE="$(find "$FEEDS_PACKAGES" -maxdepth 3 -type f -wholename '*/tailscale/Makefile' -print -quit 2>/dev/null)"
 if [ -f "$TS_FILE" ]; then
 	echo " "
-	sed -i "/PKG_VERSION:=/cPKG_VERSION:=1.94.2" $TS_FILE
 #	sed -i "/PKG_RELEASE:=/cPKG_RELEASE:=1" $TS_FILE
-	sed -i "/PKG_HASH:=/cPKG_HASH:=c45975beb4cb7bab8047cfba77ec8b170570d184f3c806258844f3e49c60d7aa" $TS_FILE
-	echo " " && echo "tailscale 使用1.94.2版本"	
+	#sed -i "/PKG_VERSION:=/cPKG_VERSION:=1.94.2" $TS_FILE
+	#sed -i "/PKG_HASH:=/cPKG_HASH:=c45975beb4cb7bab8047cfba77ec8b170570d184f3c806258844f3e49c60d7aa" $TS_FILE
+	#echo " " && echo "tailscale 使用1.94.2版本"	
 	if sed -i '/\/files/d' "$TS_FILE"; then
 		echo "tailscale has been fixed!"
 	    cat $TS_FILE
